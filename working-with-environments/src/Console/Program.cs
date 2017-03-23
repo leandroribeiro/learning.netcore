68db6f9c26fb63750bb9107e754aacf46c16bf09
Leandro Ribeiro <lrsilva@acessocard.com.br>
2017-03-23 20:06:01 -0300

    Testando passagem de parametros para console

--- working-with-environments/src/Console/Program.cs
+++ working-with-environments/src/Console/Program.cs
@@ -11,12 +11,9 @@ namespace Consolecls
 
         public static Dictionary<string, string> GetSwitchMappings(IReadOnlyDictionary<string, string> configurationStrings)
         {
-            return configurationStrings.Select(item =>
-                new KeyValuePair<string, string>(
-                    "-" + item.Key.Substring(item.Key.LastIndexOf(':') + 1),
-                    item.Key))
-                    .ToDictionary(
-                        item => item.Key, item => item.Value);
+            return configurationStrings
+                .Select(item => new KeyValuePair<string, string>("-" + item.Key.Substring(item.Key.LastIndexOf(':') + 1), item.Key))
+                .ToDictionary(item => item.Key, item => item.Value);
         }
 
         public static void Main(string[] args = null)
@@ -28,7 +25,8 @@ namespace Consolecls
                 };
 
             var builder = new ConfigurationBuilder();
-            builder.AddInMemoryCollection(dict)
+            builder
+                .AddInMemoryCollection(dict)
                 .AddCommandLine(args, GetSwitchMappings(dict));
             Configuration = builder.Build();
             Console.WriteLine($"Hello {Configuration["Profile:MachineName"]}");
---
+++ working-with-environments/src/Console2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.CommandLineUtils;
+
+namespace Console2
+{
+    class Program
+    {
+        static void Main(params string[] args)
+        {
+            // Program.exe <-g|--greeting|-$ <greeting>> [name <fullname>]
+            // [-?|-h|--help] [-u|--uppercase]
+            var commandLineApplication = new CommandLineApplication(throwOnUnexpectedArg: false);
+            CommandArgument names = null;
+            commandLineApplication.Command("name", (target) => names = target.Argument("fullname", "Enter the full name of the person to be greeted.", multipleValues: true));
+
+            var greeting = commandLineApplication.Option(
+                "-$|-g |--greeting <greeting>",
+                "The greeting to display. The greeting supports"
+                + " a format string where {fullname} will be "
+                + "substituted with the full name.",
+                CommandOptionType.SingleValue);
+
+            var uppercase = commandLineApplication.Option("-u | --uppercase", "Display the greeting in uppercase.", CommandOptionType.NoValue);
+
+            commandLineApplication.HelpOption("-? | -h | --help");
+            commandLineApplication.OnExecute(() =>
+            {
+                if (greeting.HasValue())
+                {
+                    Greet(greeting.Value(), names.Values, uppercase.HasValue());
+                }
+                return 0;
+            });
+
+            commandLineApplication.Execute(args);
+
+        }
+
+        private static void Greet(
+  string greeting, IEnumerable<string> values, bool useUppercase)
+        {
+            Console.WriteLine(greeting);
+        }
+
+    }
+
+}
